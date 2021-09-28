5b5686c5bb8955799fabec1df801a4467316ce54
paulohfev <phfernandesev@gmail.com>
2021-09-28 19:29:39 -0300

    Add Leap exercise

--- resistor-color-duo/resistor-color-duo.ts
+++ resistor-color-duo/resistor-color-duo.ts
@@ -1,5 +1,24 @@
-const COLORS: string[] = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white'];
+// const COLORS: string[] = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white'];
 
-export function decodedValue([firstCol, secondCol]: string[]): number {
+// export function decodedValue([firstCol, secondCol]: string[]): number {
+//   return (COLORS.indexOf(firstCol) * 10) + COLORS.indexOf(secondCol);
+// }
+
+const COLORS =
+  [`black`,
+    `brown`,
+    `red`,
+    `orange`,
+    `yellow`,
+    `green`,
+    `blue`,
+    `violet`,
+    `grey`,
+    `white`,] as const
+
+// this is to disallow passing e.g. 'pink' to constructor
+export type Color = typeof COLORS[number]
+
+export function decodedValue([firstCol, secondCol]: Color[]): number {
   return (COLORS.indexOf(firstCol) * 10) + COLORS.indexOf(secondCol);
 }
